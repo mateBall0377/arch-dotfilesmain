a71b0f2623100eaef14e8ecbb92686f3686b3128
mateBall0377 <73995228+mateBall0377@users.noreply.github.com>
2026-04-30 00:54:52 +0300

    Update boot.sh

--- boot.sh
+++ boot.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 BASE="https://raw.githubusercontent.com/mateBall0377/arch-dotfilesmain/master"
-
-echo "Качаю установщик..."
 curl -fsSL "$BASE/install.sh"      -o install.sh
 curl -fsSL "$BASE/chroot-setup.sh" -o chroot-setup.sh
-chmod +x install.sh chroot-setup.sh
-
+curl -fsSL "$BASE/first-boot.sh"   -o first-boot.sh
+chmod +x install.sh chroot-setup.sh first-boot.sh
 bash install.sh
